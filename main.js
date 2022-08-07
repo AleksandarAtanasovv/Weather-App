c371f209f1bdd83fac28d5c24cea0347cf802afa
Sakinaa782 <sashotrupa@hotmail.com>
2022-08-07 20:04:23 +0300

    Updatet scripts

--- main.js
+++ main.js
@@ -14,17 +14,28 @@ async function getSetWeather(location){
     const day = document.getElementById('day')
     let currentDate = new Date();
     currentDate = currentDate.toString();
+    console.log(responseData);
     let arrayDate = currentDate.split(' ');
     date.textContent = `${arrayDate[1]} ${arrayDate[2]} ${arrayDate[3]}`;
-    day.textContent = arrayDate[0]
-    console.log(arrayDate);
-    
+    if(arrayDate[0] == 'Mon'){
+        day.textContent = "Monday"
+    }else if(arrayDate[0] == 'Tue' || arrayDate[0] == 'Tues'){
+        day.textContent = 'Tuesday'
+    }else if(arrayDate[0] =='Wed'){
+        day.textContent = 'Wednesday'
+    }else if(arrayDate[0] == 'Thu' || arrayDate[0] == 'Thur' || arrayDate[0] == 'Thurs'){
+        day.textContent = 'Thursday'
+    }else if(arrayDate[0] == 'Fri'){
+        day.textContent = 'Friday'
+    }else if(arrayDate[0] == 'Sat'){
+        day.textContent = "Saturday"
+    }
     humid.textContent = `${responseData.main.humidity} %`
     win.textContent = `${Math.ceil(responseData.wind.speed)} Km/h`
     desc.textContent = `${responseData.weather[0].main}`
     degrees.textContent = `${Math.trunc(responseData.main.temp - 273.15)} °C`
     city.innerHTML = `
-    <i class="fas fa-map-marker-alt"></i> ${responseData.name}`
+    <i class="fas fa-map-marker-alt"></i> ${responseData.name}, ${responseData.sys.country}`
 }
 
 const citySearch = document.getElementById('city-search');
@@ -33,50 +44,6 @@ citySearch.addEventListener('submit', (e) =>{
     
     e.preventDefault();
     const location = search.value
-    console.log(location);
+    getSetWeather(location)
     
 })
-getSetWeather("Istanbul")
-// {
-//     "coord": {
-//         "lon": 27.4678,
-//         "lat": 42.5061
-//     },
-//     "weather": [
-//         {
-//             "id": 800,
-//             "main": "Clear",
-//             "description": "clear sky",
-//             "icon": "01d"
-//         }
-//     ],
-//     "base": "stations",
-//     "main": {
-//         "temp": 304.21,
-//         "feels_like": 303.25,
-//         "temp_min": 304.21,
-//         "temp_max": 304.21,
-//         "pressure": 1010,
-//         "humidity": 33
-//     },
-//     "visibility": 10000,
-//     "wind": {
-//         "speed": 8.75,
-//         "deg": 70
-//     },
-//     "clouds": {
-//         "all": 0
-//     },
-//     "dt": 1659889094,
-//     "sys": {
-//         "type": 1,
-//         "id": 6360,
-//         "country": "BG",
-//         "sunrise": 1659841693,
-//         "sunset": 1659893020
-//     },
-//     "timezone": 10800,
-//     "id": 732770,
-//     "name": "Burgas",
-//     "cod": 200
-// }
